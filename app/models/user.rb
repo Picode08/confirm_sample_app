ef209b09aa94702c81b319e281e9372fe1f5a188
abineko2 <yuki1980426@gmail.com>
2021-03-12 10:19:32 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
     has_many :everychecks
 
-    #ユーザーが食べた食品のレコード抽出
+    #ユーザーが食べた食品のレコード抽出##
     def user_eattings(eats)
         #食べたものリスト
         eattings = []      
 
-        #食べたものパラメーター分オブジェクト作成ただしconfirmなのでsaveはまでしない
+        #食べたものパラメーター分オブジェクト作成ただしconfirmなのでsaveはまだしない
         eats.each do |eat|      
            eatInstance = self.everychecks.new(
               foodname: eat[:foodname], 
